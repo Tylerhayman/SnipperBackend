764817742ad7aaec626289c652f638c59dd18d8d
Tyler <128461465+Tylerhayman@users.noreply.github.com>
2023-10-26 09:06:04 +0000

    created encryption

--- app.js
+++ app.js
@@ -1,40 +1,94 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 const app = express();
 
-app.use(bodyParser.json());
 
+const SECRET_KEY = 'tylerkey';
+const users = [];
 
-const snippets = require('./seedData.json'); 
+app.use(express.json());
 
+// snippets code
+const snippets = [];
 
+// create a new snippet
 app.post('/snippet', (req, res) => {
   const snippet = req.body;
-  snippets.push(snippet);
+  const encryptedCode = encrypt(snippet.code);
+
+  snippets.push({ ...snippet, code: encryptedCode });
   res.status(201).json(snippet);
 });
 
-
+// retrieve snippets
 app.get('/snippet', (req, res) => {
-  console.log(req.query); // Log the query parameters
-  const lang = req.query.lang;
-  console.log(lang); // Log the 'lang' parameter
-  // Use the lang parameter to filter snippets or perform other actions
-  res.json(snippets);
+  const decryptedSnippets = snippets.map(snippet => ({
+    ...snippet,
+    code: decrypt(snippet.code),
+  }));
+  res.json(decryptedSnippets);
 });
 
-
-
+// retrieve ID
 app.get('/snippet/:id', (req, res) => {
   const id = req.params.id;
   const snippet = snippets.find((s) => s.id === id);
+
   if (snippet) {
-    res.json(snippet);
+    const decryptedCode = decrypt(snippet.code);
+    res.json({ ...snippet, code: decryptedCode });
   } else {
     res.status(404).json({ error: 'Snippet not found' });
   }
 });
 
+// create a new user with email and password
+app.post('/user', (req, res) => {
+  const { email, password } = req.body;
+
+  // Hash and salt
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      res.status(500).json({ error: 'Password hashing failed' });
+    } else {
+      users.push({ email, password: hash });
+      res.status(201).json({ email });
+    }
+  });
+});
+
+// authenticate a user
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((u) => u.email === email);
+
+  if (!user) {
+    res.status(401).json({ error: 'Authentication failed' });
+  } else {
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (result) {
+        res.json({ email });
+      } else {
+        res.status(401).json({ error: 'Authentication failed' });
+      }
+    });
+  }
+});
+
+const encrypt = (data) => {
+  const cipher = crypto.createCipher('aes-256-cbc', SECRET_KEY);
+  let encrypted = cipher.update(data, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return encrypted;
+};
+
+const decrypt = (data) => {
+  const decipher = crypto.createDecipher('aes-256-cbc', SECRET_KEY);
+  let decrypted = decipher.update(data, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+};
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
